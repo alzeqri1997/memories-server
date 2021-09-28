c3803181ffb0eff4632b27f3ce5762dedcdd8c55
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-09-28 20:02:47 +0300

    get Posts

---
+++ controllers/posts.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+import { PostMessage } from '../models/index.js';
+
+const router = express.Router();
+
+export const getPosts = async (req, res) => {
+  const { page } = req.query;
+
+  try {
+    const LIMIT = 8;
+    const startIndex = (Number(page) - 1) * LIMIT; // Get the starting index of every page
+
+    const total = await PostMessage.countDocuments({});
+    const posts = await PostMessage.find()
+      .sort({ _id: -1 })
+      .limit(LIMIT)
+      .skip(startIndex);
+
+    res.status(200).json({
+      data: posts,
+      currentPage: Number(page),
+      numberOfPages: Math.ceil(total / LIMIT),
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export default router;
