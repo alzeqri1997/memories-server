6f00c4b98be33eb4789b323edc9ad2db5b550ec8
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-09-29 09:40:45 +0300

    update Post

--- controllers/posts.js
+++ controllers/posts.js
@@ -86,4 +86,21 @@ export const createPost = async (req, res) => {
     res.status(409).json({ message: error.message });
   }
 };
+
+export const updatePost = async (req, res) => {
+  const { id } = req.params;
+  const { title, message, creator, selectedFile, tags } = req.body;
+
+  console.log(id);
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
+
+  const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+  await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+  res.json({ updatedPost });
+};
+
 export default router;
--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
-import { createPost, getPosts } from '../controllers/posts.js';
+import { createPost, getPosts, updatePost } from '../controllers/posts.js';
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/', auth, getPosts);
 router.post('/', createPost);
+
+router.patch('/:id', auth, updatePost);
 
 export default router;
