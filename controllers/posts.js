732a9a8cd9ee9090f60cc7fc4ce21c2ce17fe67d
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-09-29 17:38:17 +0300

    add likePost & commentPost

--- controllers/posts.js
+++ controllers/posts.js
@@ -7,24 +7,29 @@ const router = express.Router();
 
 export const getPosts = async (req, res) => {
   const { page } = req.query;
+  console.log(page);
 
   try {
     const LIMIT = 8;
     const startIndex = (Number(page) - 1) * LIMIT; // Get the starting index of every page
 
     const total = await PostMessage.countDocuments({});
+
     const posts = await PostMessage.find()
       .sort({ _id: -1 })
       .limit(LIMIT)
       .skip(startIndex);
 
+    // const posts = await PostMessage.find();
+
+    console.log(posts);
     res.status(200).json({
       data: posts,
       currentPage: Number(page),
       numberOfPages: Math.ceil(total / LIMIT),
     });
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    res.status(404).json({ message: 'error occurred', error });
   }
 };
 
@@ -114,4 +119,45 @@ export const deletePost = async (req, res) => {
   res.json({ message: 'Post deleted successfully' });
 };
 
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!req.userId) {
+    return res.json({ message: 'Unauthenticated' });
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id : ${id}`);
+
+  const post = await PostMessage.findById(id);
+
+  const index = post.likes.findIndex((id) => id === String(req.userId));
+
+  if (index === -1) {
+    post.likes.push(req.userId);
+  } else {
+    post.likes = post.likes.filter((id) => id !== String(req.userId));
+  }
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
+    new: true,
+  });
+
+  res.status(200).json(updatedPost);
+};
+
+export const commentPost = async (req, res) => {
+  const { id } = req.params;
+  const { value } = req.body;
+
+  const post = await PostMessage.findById(id);
+
+  post.comments.push(value);
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
+    new: true,
+  });
+
+  res.json(updatedPost);
+};
 export default router;
