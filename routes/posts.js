a70faa445b3d757a4ba0c06de89c7bf3808bea6a
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-09-29 09:44:21 +0300

    delete post

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,11 @@
 import express from 'express';
 
-import { createPost, getPosts, updatePost } from '../controllers/posts.js';
+import {
+  createPost,
+  getPosts,
+  updatePost,
+  deletePost,
+} from '../controllers/posts.js';
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
@@ -9,5 +14,6 @@ router.get('/', auth, getPosts);
 router.post('/', createPost);
 
 router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
 
 export default router;
