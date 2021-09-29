3eac6c44bfda6fba8b0f6e39cd2ca3e530828eab
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-09-29 17:37:16 +0300

    add signIn function

--- controllers/user.js
+++ controllers/user.js
@@ -8,6 +8,30 @@ import UserModel from '../models/user.js';
 
 const secret = process.env.SECRET;
 
+export const signIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const oldUser = await UserModel.findOne({ email });
+
+    if (!oldUser)
+      return res.status(404).json({ message: "User doesn't exist" });
+
+    const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
+
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: 'Invalid Password' });
+
+    const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {
+      expiresIn: '1h',
+    });
+
+    res.status(200).json({ result: oldUser, token });
+  } catch (err) {
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+};
+
 export const signup = async (req, res) => {
   const { email, password, firstName, lastName } = req.body;
 
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
 
-import { signup } from '../controllers/user.js';
+import { signup, signIn } from '../controllers/user.js';
 
 router.post('/signup', signup);
+router.post('/signin', signIn);
 
 export default router;
